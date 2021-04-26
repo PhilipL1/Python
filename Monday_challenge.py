b8c517ddf559cf4fb2d65b16366d1c2f49076d0d
Philip <Philiplartey@hotmail.com>
2021-04-26 17:15:34 +0000

    challenge

---
+++ Monday_challenge.py
@@ -0,0 +1,16 @@
+
+
+
+def even_number(x,y):
+    items = []
+    for i in range(x,y):
+        s = str(i)
+        if (int(s[0])%2==0) and (int(s[1])%2==0): 
+            items.append(s)
+    return items
+
+print(even_number(10,30))
+
+
+    
+    
