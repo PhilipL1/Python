3b47e5d67e6d605967dffa94e032a7d9d2d2e5aa
Philip <Philiplartey@hotmail.com>
2021-04-26 13:10:31 +0000

    function file

---
+++ Functions.py
@@ -0,0 +1,12 @@
+input_name = input("name: ")
+input_homework = int(input(" Homework: "))
+input_assessment= int(input(" Assessment: "))
+input_exam = int(input(" Final Exam: "))
+
+def final_grade(homework, assessment, exam):
+    a= (homework/25)*100
+    b= (assessment/50)*100
+    c= (exam/100)*100
+    return((a *0.25)+ (b*0.5) + c) /1.75
+   
+print( input_name + "'s final grade is :  " + str(final_grade(input_homework, input_homework, input_exam)))
