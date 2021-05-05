5ce35f47c3b782d4517bf361a972aa58e0721eeb
Philip <Philiplartey@hotmail.com>
2021-05-05 16:05:42 +0000

    05/05/2021

---
+++ Python-Challenges /Ollie examples/example4.py
@@ -0,0 +1,16 @@
+ # <QUESTION 4>
+
+    # Given an integer, return the largest of the numbers this integer is divisible by
+    # excluding itself
+
+    # This should also work for negative numbers
+
+    # <EXAMPLES>
+
+    # four(10) → 5
+    # four(24) → 12
+    # four(7) → 1
+    # four(-10) → 5
+
+def four(number):
+    pass
